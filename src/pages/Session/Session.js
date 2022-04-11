6269e557a53703086ab88b1cd7b5151f24e0eec8
Diana <diana45637@live.com>
2022-04-11 12:54:18 -0300

    criando o session

---
+++ src/pages/Session/Session.js
@@ -0,0 +1,88 @@
+import { InputWrapper, Input , Select, NumberInput, Textarea, Button } from "@mantine/core";
+import { useState } from "react";
+
+const initialState = {
+    name: "",
+    description: "",
+    classification: "",
+    duration: 0,
+};
+
+const Session = ({session = initialState, onSubmit}) =>{ 
+
+    const [form, setForm] = useState(session);
+
+    const onChange = (event) => {
+        const {
+        target: { name, type, checked, value },
+        } = event;
+
+        setForm((prevForm) => ({
+        ...prevForm,
+        [name]: type === "checkbox" ? checked : value,
+        }));
+    };
+    
+    return (
+    <>
+        <InputWrapper
+        id="name"
+        mb={8}
+        required
+        label="Movie Name"
+        >
+        <Input
+            id="name"
+            name="name"
+            value={form.name}
+            onChange={onChange}
+        />
+        </InputWrapper>
+
+        <InputWrapper
+        id="description"
+        mb={8}
+        required
+        label="Description"
+        >
+        <Textarea
+            id="description"
+            name="description"
+            value={form.description}
+            onChange={onChange}
+        />
+        </InputWrapper>
+        <Select
+            mb={8}
+            required
+            label="Classification"
+            placeholder="Pick one"
+            value={form.classification}
+            onChange={(value) => onChange({target: {name: "classification", value}})}
+            data={[
+                {
+                    value: "GENERAL_AUDIENCE",
+                    label: "GENERAL_AUDIENCE"
+                },
+                {
+                    value: "PARENT_GUIDANCE_SUGGESTED",
+                    label: "PARENT_GUIDANCE_SUGGESTED",
+                },
+                {   value: "RESTRICTED", label: "RESTRICTED" },
+            ]}
+        />
+        <NumberInput
+            id="duration"
+            mb={8}
+            required
+            label="Duration"
+            value={form.duration}
+            onChange={(value) => onChange({target: {name: "duration", value}})}
+        />
+        <Button fullWidth onClick={() => onSubmit(form)}>
+            Submit
+        </Button>
+    </>
+    )
+}
+export default Session;
--- src/pages/Session/index.js
+++ src/pages/Session/index.js
@@ -1,48 +1,99 @@
-import ListView from "../../components/ListView"
+import { useModals } from '@mantine/modals';
+import { showNotification } from '@mantine/notifications';
+import {useState} from "react";
 
-const Session = () => (
-    <ListView
-    columns={[
-        {
-            key:  "id",
-            value: "Id",
-        },
-        {
-            key:  "movieId",
-            render: (movie) => movie.name,
-            value: "Movie",
-        },
-        {
-            key: "sessionDate",
-            value: "Session Date",
-          },
-        {
-            key:  "room",
-            value: "Room",
-        },
-        // {
-        //     key:  "SessionSeats",
-        //     render: (sessionSeats) => {
-        //         const availableSeats = sessionSeats.filter(
-        //             ({state}) => state === "AVAILABLE"
-        //         );
-        //         return `${availableSeats.length}/${sessionSeats.length}`;
-        //     },
-        //     value: "Availability Seats",
-        // },
-        {
-            key:  "price",
-            render: (price) =>
-            new Intl.NumberFormat("pt-BR", {
-              style: "currency",
-              currency: "BRL",
-            }).format(price),
-            value: "Price",
-        },
-    ]}
-    endpoint="/session"
-    title="Session"
-    />
-);
+import SessionForm from './Session';
+import ListView from "../../components/ListView";
+import axios from '../../services/api';
+
+const Session = () => { 
+    
+    const [lastSessionTimestamp, setLastSessionTimestamp] = useState();
+    const modals = useModals();
+
+    const onSubmit = (modalId) => async (form) => {
+        try {
+            //para fazer put e não post
+            if(form.id) {
+                await axios.put(`/session/${form.id}`, form);
+            } else {
+                await axios.post("/session", form);
+            }
+            
+            showNotification({
+                title: "Success",
+                message: `Session ${form.id ? "Updated" : "Created"} with Success`,
+                color: "green"
+            });
+            modals.closeModal(modalId);
+            setLastSessionTimestamp(new Date().getTime());
+        } catch (error) {
+            showNotification({
+                title: "Error",
+                message: error.response.data.message,
+                color: "red"
+            });
+        }
+    }
+
+    const openContentModal = (session) => {
+        const id = modals.openModal({
+          title: `${session?.id ? "Update" : "Create" } Session`,
+          size: "xl",
+          children: (
+            <>
+              <SessionForm session={session} onSubmit={(form) => onSubmit(id)(form)}/>
+            </>
+          ),
+        });
+      };
+
+    return (
+        <ListView
+        columns={[
+            {
+                key:  "id",
+                value: "Id",
+            },
+            {
+                key:  "movieId",
+                render: (movie) => movie.name,
+                value: "Movie",
+            },
+            {
+                key: "sessionDate",
+                value: "Session Date",
+            },
+            {
+                key:  "room",
+                value: "Room",
+            },
+            // {
+            //     key:  "SessionSeats",
+            //     render: (sessionSeats) => {
+            //         const availableSeats = sessionSeats.filter(
+            //             ({state}) => state === "AVAILABLE"
+            //         );
+            //         return `${availableSeats.length}/${sessionSeats.length}`;
+            //     },
+            //     value: "Availability Seats",
+            // },
+            {
+                key:  "price",
+                render: (price) =>
+                new Intl.NumberFormat("pt-BR", {
+                style: "currency",
+                currency: "BRL",
+                }).format(price),
+                value: "Price",
+            },
+        ]}
+        endpoint="/session"
+        title="Session"
+        openContentModal={openContentModal}
+        lastSessionTimestamp={lastSessionTimestamp}
+        />
+    );
+};
 
 export default Session;
